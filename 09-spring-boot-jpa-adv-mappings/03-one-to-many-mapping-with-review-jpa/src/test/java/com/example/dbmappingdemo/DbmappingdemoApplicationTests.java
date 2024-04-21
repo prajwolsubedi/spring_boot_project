c18ad5ac36bedb8cde6734b0ef5589b06b646636
prajwol subedi <prajwolsubedizzz@gmail.com>
2024-04-21 20:05:43 +0545

    update

---
+++ 09-spring-boot-jpa-adv-mappings/03-one-to-many-mapping-with-review-jpa/src/test/java/com/example/dbmappingdemo/DbmappingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbmappingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmappingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
